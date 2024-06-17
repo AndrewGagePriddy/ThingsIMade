807cf2364233e925683e1745f1eba04099154db3
AndrewGagePriddy <andrewgagepriddy@gmail.com>
2024-06-17 18:11:40 -0500

    Update sortCodeWord.py

--- sortCodeWord.py
+++ sortCodeWord.py
@@ -1,16 +1,22 @@
 import re
 
+#pulls text in from a text file
 t = open("C:\\Users\\cathe\\PythonProjects\\TextIn.txt", "r")
 
+#puts text in a string
 myString = t.read()
 
+#Splits sting into an array
 myList = re.split('\n',myString)
 
+#Defines nartural sort
 def natural_key(string_):
     return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
 
+#Sorts with natural sort
 myList.sort(key = natural_key)
 
+#Create a pyramid with the words
 def create_pyramid(nums):
  rows = int(len(myList))
  k = round(rows/2)
@@ -26,6 +32,7 @@ def create_pyramid(nums):
          print(myList[countwords],end=" ")
          countwords+=1
      g=1
+
 
 create_pyramid(myList)
 
