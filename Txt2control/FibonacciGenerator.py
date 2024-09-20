e5a4ab8efe06119a029b25fe68a095ff3191232b
AndrewGagePriddy <andrewgagepriddy@gmail.com>
2024-09-20 12:33:56 -0500

    Add files via upload

---
+++ Txt2control/FibonacciGenerator.py
@@ -0,0 +1,20 @@
+
+txtOut =r"\TextFile\ReadThis.txt"
+def Fib(i):
+	f1 = 0
+	f2 = 1
+	f = open(txtOut,"w")
+	
+	for t in range(i):
+			
+		#this line of code will write it out vertically
+		#txt = f"{f1} \n"
+		f.write(str(f1))
+		f3 = f1 + f2
+		f1 = f2
+		f2 = f3
+			
+			
+			
+		
+	f.close()
